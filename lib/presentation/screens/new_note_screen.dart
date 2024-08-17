cff07630333eb0ef3766d8379d8c7abc183c6e34
RafBel94 <rafael.beltrancaceres@gmail.com>
2024-08-17 12:05:57 +0200

    New screen in process to create new notes

---
+++ lib/presentation/screens/new_note_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewNoteScreen extends StatelessWidget {
+  const NewNoteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 254, 204, 54),
+        centerTitle: true,
+        title: Text('New Note'),
+      ),
+    );
+  }
+}
