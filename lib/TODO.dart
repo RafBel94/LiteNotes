3abcda650cd8f9347d9c1defc7945882af2d6b41
RafBel94 <rafael.beltrancaceres@gmail.com>
2024-08-20 17:06:46 +0200

    Update on TODO

--- lib/TODO.dart
+++ lib/TODO.dart
@@ -1,2 +1,8 @@
 // TODO
 // - Make a warning appear to confirm the delete of a note
+// - Colored notes
+// - Tagged notes
+// - Recicle bin
+// - Notes inside the recible bin will be deleted after 30 days
+// - Select notes for deleting all at the same time
+// - Add last modified date for notes
