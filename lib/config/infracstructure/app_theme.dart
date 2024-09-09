8c645b3f696ee06ed092d8eae7841aa09ed808c5
RafBel94 <rafael.beltrancaceres@gmail.com>
2024-09-09 13:54:23 +0200

    Forced portrait mode
    Now the app can only be used in portrait orientation ( Vertical )

--- lib/config/infracstructure/app_theme.dart
+++ lib/config/infracstructure/app_theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const List<Color> _colorList = [Color.fromRGBO(192, 150, 25, 1), Color.fromARGB(255, 40, 234, 215), Color.fromRGBO(172, 124, 255, 1)];
+const List<Color> _colorList = [Color.fromRGBO(192, 150, 25, 1), Color.fromARGB(255, 60, 60, 60), Color.fromRGBO(172, 124, 255, 1)];
 
 class AppTheme {
   final int selectedColor;
